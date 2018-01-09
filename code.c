9ef523d5a407684c4ebd9f87afe288fa74034310
Ambika121 <35267406+Ambika121@users.noreply.github.com>
2018-01-09 23:04:52 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,13 @@
-
+include<stdio.h>
+void main()
+{
+  int num;
+  printf("Enter the number");
+  scanf("%d",&num);
+  if(num>0)
+    printf("Number is Positive");
+  else if(num<0)
+    priintf("Number is Negative");
+  else
+    printf("Number is Zero");
+}
