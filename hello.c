f2c533086870826f0b4572aa637cf8d9091131f3
Ambika121 <35267406+Ambika121@users.noreply.github.com>
2018-01-15 00:15:34 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    int n,i;
+    printf("Enter the limit");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    	printf("Hello\n");
+    }
+    return 0;
+}
